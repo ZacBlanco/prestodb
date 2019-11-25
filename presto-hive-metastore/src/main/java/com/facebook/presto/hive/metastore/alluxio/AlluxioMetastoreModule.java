1667c326b2f3e8b4f9c15bfca35855706e0c2391
Zac Blanco <zac@alluxio.com>
2019-11-25 18:25:34 -0500

    Port PrestoSQL connector code to PrestoDB
    This commit copies the code from the PrestoSQL connector
    into the prestoDB codebase. The files have been placed
    in the proper maven module in this source tree.
    
    The high-level  differences are outlined below
    
    - The AlluxioHiveMetastore, AlluxioMetastoreModule, and
    ProtoUtils classes does not yet compile
    - The plugin jar creation was changed from
    presto-server/src/provisio to presto-server/src/assembly
    
    A follow-up commit will address the compilation issues.

---
+++ presto-hive-metastore/src/main/java/com/facebook/presto/hive/metastore/alluxio/AlluxioMetastoreModule.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.plugin.hive.metastore.alluxio;
+
+import alluxio.ClientContext;
+import alluxio.client.table.RetryHandlingTableMasterClient;
+import alluxio.client.table.TableMasterClient;
+import alluxio.conf.InstancedConfiguration;
+import alluxio.master.MasterClientContext;
+import alluxio.util.ConfigurationUtils;
+
+import com.facebook.airlift.configuration.AbstractConfigurationAwareModule;
+import com.facebook.presto.hive.metastore.alluxio.AlluxioHiveMetastore;
+import com.facebook.presto.hive.metastore.alluxio.AlluxioHiveMetastoreConfig;
+import com.facebook.presto.hive.metastore.thrift.HiveMetastore;
+import com.google.inject.Binder;
+import com.google.inject.Provides;
+import com.google.inject.Scopes;
+
+import static com.facebook.airlift.configuration.ConfigBinder.configBinder;
+import static org.weakref.jmx.ObjectNames.generatedNameOf;
+import static org.weakref.jmx.guice.ExportBinder.newExporter;
+
+/**
+ * Module for an Alluxio metastore implementation of the {@link HiveMetastore} interface.
+ */
+public class AlluxioMetastoreModule
+         extends AbstractConfigurationAwareModule
+{
+    @Override
+    protected void setup(Binder binder)
+    {
+        configBinder(binder).bindConfig(AlluxioHiveMetastoreConfig.class);
+
+        binder.bind(HiveMetastore.class).to(AlluxioHiveMetastore.class).in(Scopes.SINGLETON);
+        newExporter(binder).export(HiveMetastore.class).as(generated -> generated.generatedNameOf(AlluxioHiveMetastore.class));
+    }
+
+    @Provides
+    TableMasterClient provideCatalogMasterClient()
+    {
+        MasterClientContext context = MasterClientContext
+                .newBuilder(ClientContext.create(new InstancedConfiguration(ConfigurationUtils.defaults()))).build();
+        return new RetryHandlingTableMasterClient(context);
+    }
+}
