e510882b8d28675a1de5be9416a5deab5bdfb3c5
Zac Blanco <zac@ibm.com>
2023-07-17 11:26:21 -0700

    Add functions to calculate the population size

--- presto-iceberg/src/main/java/com/facebook/presto/iceberg/samples/SampleUtil.java
+++ presto-iceberg/src/main/java/com/facebook/presto/iceberg/samples/SampleUtil.java
@@ -21,6 +21,8 @@ import com.facebook.presto.iceberg.IcebergResourceFactory;
 import com.facebook.presto.spi.ConnectorSession;
 import com.facebook.presto.spi.PrestoException;
 import com.facebook.presto.spi.SchemaTableName;
+import com.google.common.base.Preconditions;
+import org.apache.commons.math3.distribution.NormalDistribution;
 import org.apache.hadoop.fs.Path;
 import org.apache.iceberg.Table;
 import org.apache.iceberg.catalog.TableIdentifier;
@@ -78,6 +80,66 @@ public class SampleUtil
         try (AutoCloseableCatalog c = getCatalogForSampleTable(icebergSource, prestoSchema, env, session)) {
             return c.loadTable(SAMPLE_TABLE_ID);
         }
+    }
+
+    /**
+     * Standard normal distribution used to calculate scores which are used to determine sample
+     * sizes.
+     */
+    private static final NormalDistribution NORMAL_DISTRIBUTION = new NormalDistribution();
+
+    /**
+     * Roughly 1 quintillion.
+     */
+    private static final long MAX_POPULATION_SIZE = 1_000_000_000_000_000L;
+
+    /**
+     * Calculates the size of the sample required to achieve a mean within the sample
+     * at the given confidence and margin of error for the provided population size.
+     * <br>
+     * If the population size becomes greater than roughly 1 sextillion, the ability for us to
+     * recompute the original population size from the sample size and input parameters decreases.
+     * In that case, we will assume the population size to be infinite for the calculations
+     *
+     * @param confidence a value between (0, 1.0)
+     * @param error a value between (0, 1.0)
+     * @param populationSize a value > 0.
+     * @return the sample size
+     */
+    public static long calculateSampleSizeFromConfidence(double confidence, double error, long populationSize)
+    {
+        Preconditions.checkArgument(confidence > 0.0 && confidence < 1.0, "confidence must be in the range (0.0, 1.0)");
+        Preconditions.checkArgument(error > 0.0 && error < 1.0, "error must be in the range (0.0, 1.0)");
+        Preconditions.checkArgument(populationSize > 0, "population size must be > 0");
+        double m = calculateMValue(confidence, error);
+        if (populationSize > MAX_POPULATION_SIZE) {
+            return (long) Math.ceil(m);
+        }
+        double sampleSize = m / (1 + ((m - 1) / populationSize));
+        return (long) Math.ceil(sampleSize);
+    }
+
+    /**
+     * With a given sample size, and original confidence and error bounds used to create the sample,
+     * reverse the calculation to determine the original population size.
+     *
+     * @param confidence the confidence interval used to generate the sample
+     * @param error the margin of error used to generate the sample
+     * @param sampleSize the size of the sample
+     * @return the original population size of the sample
+     */
+    public static long calculatePopulationSizeFromSample(double confidence, double error, long sampleSize)
+    {
+        Preconditions.checkArgument(confidence > 0.0 && confidence < 1.0, "confidence must be in the range (0.0, 1.0)");
+        Preconditions.checkArgument(error > 0.0 && error < 1.0, "error must be in the range (0.0, 1.0)");
+        Preconditions.checkArgument(sampleSize > 0, "population size must be > 0");
+        double m = calculateMValue(confidence, error);
+        return (long)((sampleSize * (m - 1)) / (m - sampleSize));
+    }
+
+    public static double calculateMValue(double confidence, double error) {
+        double zScore = NORMAL_DISTRIBUTION.inverseCumulativeProbability(confidence + ((1.0 - confidence) / 2));
+        return Math.pow(zScore, 2) / (4 * Math.pow(error, 2));
     }
 
     public static class AutoCloseableCatalog
