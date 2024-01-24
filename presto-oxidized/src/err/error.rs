4763ecb548bb4278a42e3dddb5ac94fef219d4db
Zac Blanco <zac@ibm.com>
2024-01-24 08:37:51 -0800

    Add boilerplate with actix-web server

---
+++ presto-oxidized/src/err/error.rs
@@ -0,0 +1,18 @@
+use std::fmt::{Debug, Display, Formatter};
+
+use actix_web::ResponseError;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub enum Error {
+    BackwardsTime,
+    MissingParallelism,
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", &self)
+    }
+}
+
+impl ResponseError for Error {}
