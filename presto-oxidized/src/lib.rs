62c59a825b2b73ae53dabd053539fdddbaa4dcf6
Zac Blanco <zac@ibm.com>
2024-01-25 10:26:34 -0800

    Add discovery and memory functionality
    Adds the ability to announce to the discovery server that the server is live. This includes
    a new task spawned at the start of the process which sends a hard-coded service
    announcement message every 20 seconds or so. Eventually will probably want to
    configure this. The discovery URI is currently hard-coded.
    
    In addition, this commit also implements the basic /v1/memory APIs such that the
    coordinator sees this node as a valid node to schedule tasks to. Running queries on
    the coordinator will now attempt to schedule tasks via the task control plane API
    which is currently unimplemented.

--- presto-oxidized/src/lib.rs
+++ presto-oxidized/src/lib.rs
@@ -2,4 +2,5 @@ pub mod err;
 pub mod resources;
 pub mod server;
 
+pub mod discovery;
 pub mod presto_protocol;
--- presto-oxidized/src/main.rs
+++ presto-oxidized/src/main.rs
@@ -1,20 +1,61 @@
+use std::time::Duration;
+
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 
-use prestox;
+use log::{debug, error, info, warn};
+use prestox::discovery;
 use prestox::resources::AppState;
 use prestox::server::api::worker_config;
+use prestox::{self};
+use tokio::{signal, time};
 
-#[actix_web::main]
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        let state = AppState::new().unwrap();
-        App::new().service(
-            web::scope("/v1")
-                .configure(worker_config)
-                .app_data(web::Data::new(state)),
-        )
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    let state = web::Data::new(AppState::new().unwrap());
+    let web_data = state.clone();
+    let heartbeat_data = state.clone();
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Debug)
+        .init();
+    let server_task = tokio::task::spawn(
+        HttpServer::new(move || {
+            let data = web_data.clone();
+            App::new().service(
+                web::scope("/v1")
+                    .wrap(Logger::default())
+                    .configure(worker_config)
+                    .app_data(data),
+            )
+        })
+        .bind(("127.0.0.1", 9090))?
+        .run(),
+    );
+
+    let heartbeat_task = tokio::task::spawn(async move {
+        let state = heartbeat_data;
+        let mut interval = time::interval(Duration::from_secs(15));
+        let discovery_uri = &state.node_config.coordinator_url;
+        let client = reqwest::Client::builder().build().unwrap();
+        loop {
+            interval.tick().await;
+            debug!("sending heartbeat: {:?}", state);
+            if let Err(e) = discovery::announce(&client, discovery_uri, &state.node_info).await {
+                warn!("Failed to announce to discovery server: {:?}", e);
+            } else {
+                debug!("sent announcement with {}", state.node_info.node_id);
+            }
+        }
+    });
+
+    match signal::ctrl_c().await {
+        Ok(()) => {}
+        Err(err) => {
+            error!("Failed to listen for shutdown signal: {}", err)
+        }
+    }
+    info!("Gracefully shutting down...");
+    heartbeat_task.abort();
+    server_task.abort();
+    Ok(())
 }
