42b617b5c9b3adc91928854f0ba1855dfb0cd2e0
Zac Blanco <zac@ibm.com>
2024-04-22 15:15:16 -0400

    [Iceberg] Collect superset of supported statistics
    The set of ColumnStatisticsMetadata defined by the Hive
    and non-Hive connectors are not equivalent. However, it is possible
    to collect the superset of the relevant metadata and use it for ANALYZE.
    The returned statistics just need to be filtered out to contain only
    the relevant column statistics.
    
    This may include duplicate calculations for some statistics. For
    example, with distinct values Iceberg puffin files can store the
    result of sketch_theta for distinct values, but the code path for
    storing the statistic in the HMS requires a direct value from
    approx_distinct. Thus, ANALYZE may compute a value twice.

--- presto-iceberg/src/test/java/com/facebook/presto/iceberg/IcebergQueryRunner.java
+++ presto-iceberg/src/test/java/com/facebook/presto/iceberg/IcebergQueryRunner.java
@@ -182,7 +182,7 @@ public final class IcebergQueryRunner
             queryRunner.createCatalog("jmx", "jmx");
         }
 
-        if (catalogType == HIVE.name()) {
+        if (catalogType.equals(HIVE.name())) {
             Path icebergDir = icebergDataDirectory
                     .resolve(TEST_DATA_DIRECTORY)
                     .getParent()
