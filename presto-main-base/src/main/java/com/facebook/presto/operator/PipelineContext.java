prestodb/presto

[presto-main-base] jakarta conversion
1. Moves packages to jakarta APIs
2. Removes use of jakarta.ws.rs and jakarta.servelet-api because they require java 17 and 11 respectively

Modernizer not yet completed

presto-main-base/src/main/java/com/facebook/presto/operator/PipelineContext.java
@@ -13,6 +13,7 @@
  */
 package com.facebook.presto.operator;
 
+import com.facebook.airlift.concurrent.ThreadSafe;
 import com.facebook.airlift.stats.CounterStat;
 import com.facebook.airlift.stats.Distribution;
 import com.facebook.presto.Session;
@@ -28,8 +29,6 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ListMultimap;
 import com.google.common.util.concurrent.ListenableFuture;
-
-import javax.annotation.concurrent.ThreadSafe;
 
 import java.util.Iterator;
 import java.util.List;

ZacBlanco/prestodb

presto-main-base/src/main/java/com/facebook/presto/operator/PipelineContext.java
@@ -26,8 +26,7 @@ import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.util.concurrent.ListenableFuture;
-
-import javax.annotation.concurrent.ThreadSafe;
+import com.google.errorprone.annotations.ThreadSafe;
 
 import java.util.ArrayList;
 import java.util.Arrays;